690c8f7dd2c35205ba6a1d8aac4506cc30ea6cbe
erosa@nautes-ke.it <eros.abatelli@nautes.com>
2023-05-15 20:21:16 +0200

    Modificata leggermente la UI, impostato il focus nella bottomAppBar e iniziato il dialog per aggiungere una spesa o un incasso

--- app/src/main/java/it/eros/moneymoney/views/dashboard/DashboardActivity.kt
+++ app/src/main/java/it/eros/moneymoney/views/dashboard/DashboardActivity.kt
@@ -1,16 +1,27 @@
 package it.eros.moneymoney.views.dashboard
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomappbar.BottomAppBar
-import com.google.android.material.bottomnavigation.BottomNavigationMenuView
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import it.eros.moneymoney.BaseActivity
 import it.eros.moneymoney.R
 import it.eros.moneymoney.views.dashboard.fragment.DashboardFragment
 
+
 class DashboardActivity : BaseActivity() {
 
     private lateinit var bottomAppBar: BottomAppBar
+    private lateinit var fab: FloatingActionButton
+    private lateinit var dashboard: ImageButton
+    private lateinit var stats: ImageButton
+    private lateinit var profile: ImageButton
+    private lateinit var settings: ImageButton
+    private lateinit var bottomSheetView: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +40,58 @@ class DashboardActivity : BaseActivity() {
 
     private fun initWidget() {
         bottomAppBar = findViewById(R.id.bottomAppBar)
+        fab = findViewById(R.id.fab)
+        dashboard = findViewById(R.id.dashboard)
+        stats = findViewById(R.id.stats)
+        profile = findViewById(R.id.profile)
+        settings = findViewById(R.id.settings)
 
+        bottomSheetView = layoutInflater.inflate(R.layout.activity_bottom_sheet_layout, null)
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(bottomSheetView)
+
+
+        dashboard.requestFocus()
+        dashboard.setImageWithFocus(R.drawable.ic_home_pressed, R.drawable.ic_home)
+        stats.setImageWithFocus(R.drawable.ic_stats, R.drawable.ic_stats)
+        profile.setImageWithFocus(R.drawable.ic_profile_pressed, R.drawable.ic_profile)
+        settings.setImageWithFocus(R.drawable.ic_settings_pressed, R.drawable.ic_settings)
+
+
+        dashboard.setOnClickListener {
+            changeFragment(DashboardFragment())
+        }
+
+        stats.setOnClickListener {
+        }
+
+        profile.setOnClickListener {
+        }
+
+        settings.setOnClickListener {
+        }
+
+        fab.setOnClickListener {
+            bottomSheetDialog.show()
+        }
     }
 
+
+    //-----------------------------------------------------------------------------
+
+    private fun ImageButton.setImageWithFocus(focusedImage: Int, unfocusedImage: Int) {
+        this.setOnFocusChangeListener { view, hasFocus ->
+            this.setImageResource(if (hasFocus) focusedImage else unfocusedImage)
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+
+    private fun changeFragment(fragmentToChange: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentActivity, fragmentToChange)
+            .commit()
+    }
 
 
     //-----------------------------------------------------------------------------
